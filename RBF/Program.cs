ac4ee2d172f753ac393cc5d4063cb3356653bade
Max <maxim7012@gmail.com>
2020-03-28 14:02:50 +0300

    fix

--- RBF/Program.cs
+++ RBF/Program.cs
@@ -8,10 +8,10 @@ namespace RBF
     {
 
         static double[][] XORInput = {
-            new[] {0.001, 0.0,0.1},
-            new[] {2.0, 0.0,0.1},
-            new[] {0.0, 1.0,0.1},
-            new[] {1.0, 1.0, 0.1 }
+           
+            new[] {0.0, 0.0, 0.1},
+            new[] {0.0, 0.1, 0.0},
+            new[] {0.1, 0.0, 0.0}
 
 
         };
@@ -20,7 +20,7 @@ namespace RBF
 
         static double[][] XORIdeal = {
 
-            new[] {0.0},
+          
             new[] {1.0},
             new[] {1.0},
             new[] {0.0}
@@ -28,22 +28,7 @@ namespace RBF
         };
 
 
-        public RBFN(int sizeX, params int[] W)
-        {
-            XORInput = new double[1][];
-            XORIdeal = new double[1][];
-            int sizeY = W[W.Length - 1];
-            Random rnd1 = new Random();
-            double[] XORInput_layer = new double[sizeX];
-            for (int i = 0; i < sizeX; i++) XORInput_layer[i] = rnd1.NextDouble() - 0.5;
-            XORInput[0] = XORInput_layer;
-
-
-            Random rnd2 = new Random();
-            double[] XORIdeal_layer = new double[sizeY];
-            for (int i = 0; i < sizeY; i++) XORIdeal_layer[i] = rnd2.NextDouble() - 0.5;
-            XORIdeal[0] = XORIdeal_layer;
-        }
+   
 
 
 
@@ -58,7 +43,7 @@ namespace RBF
                 double volumeNeuronWidth = 2.0 / numNeuronsPerDimension; ;
                 bool includeEdgeRBFs = true;
 
-               RBFNetwork n = new RBFNetwork(dimension, numNeuronsPerDimension, 1, RBFEnum.Gaussian);
+                RBFNetwork n = new RBFNetwork(dimension, numNeuronsPerDimension, 1, RBFEnum.Gaussian);
                 n.SetRBFCentersAndWidthsEqualSpacing(0, 1, RBFEnum.Gaussian, volumeNeuronWidth, includeEdgeRBFs);
 
 
